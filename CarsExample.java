144d7d08599b4d3dfc0d3620b02894b04a4ffe54
Eleni K <71977337+Lena341@users.noreply.github.com>
2021-09-21 21:48:08 +0300

    Add files via upload

---
+++ CarsExample.java
@@ -0,0 +1,84 @@
+package carsDatabs;
+
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
+
+public class CarsExample {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Connection con=null;
+		try
+		{
+			String url="jdbc:sqlserver://LAPTOP-B566P0U1:1433;databaseName=Cars;integratedSecurity=true; ";
+			con=DriverManager.getConnection(url);
+			if(con!=null)
+				System.out.println("Connected");
+			
+			//Insert
+			String sql="INSERT INTO Newcars (model,brand,price) VALUES (?,?,?) ";
+			PreparedStatement stm=con.prepareStatement(sql);
+			stm.setNString(1,"abc");
+			stm.setNString(2, "ght");
+			stm.setInt(3, 80000);
+			int rowsInserted=stm.executeUpdate();
+			if(rowsInserted>0)
+				System.out.println("Data were inserted successfully");
+			
+			//Select
+			sql="SELECT * FROM Newcars ";
+			Statement stmt=con.createStatement();
+			ResultSet result=stmt.executeQuery(sql);
+			int count=0;
+			while(result.next())
+			{
+				String model=result.getString("model");
+				String brand=result.getString("brand");
+				int price=result.getInt("price");
+				String output = "User #%d: %s - %s - %d";
+			    System.out.println(String.format(output, ++count, model,brand,price));
+			}
+			
+			//Update
+			sql="UPDATE Newcars SET model=?,brand=?,price=? WHERE model=?";
+			stm.setString(1, "hello");
+			stm.setString(2, "world");
+			stm.setInt(3, 35000);
+			int rowsUpdated=stm.executeUpdate();
+			if(rowsUpdated>0)
+				System.out.println("Updated");
+			
+			//Delete
+			sql="DELETE FROM Newcars WHERE model=?";
+			stm=con.prepareStatement(sql);
+			stm.setString(1,"abc");
+			int rowsDeleted=stm.executeUpdate();
+			if(rowsDeleted>0)
+				System.out.println("Deleted");
+			
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+		finally
+		{
+			try
+			{
+				if(con!=null && !con.isClosed())
+					con.close();
+			}catch(SQLException ex) {ex.printStackTrace();}
+			
+		}
+		
+
+	}
+
+}
